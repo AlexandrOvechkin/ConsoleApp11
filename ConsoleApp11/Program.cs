a991f9f82f69aea7421a54eef47d703c82651061
AlexandrOvechkin <aleksowe4kin@gmail.com>
2023-06-16 12:01:44 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+
+public static class Extensions
+{
+    public static string Reverse(this string str)
+    {
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string lines = "ABCDIFGH";
+
+         
+        if(lines.Length%2 == 0)
+        {
+            int i = lines.Length/2;
+            string first = lines[..i];
+            string second = lines[i..];
+
+             string reverseFirst = first.Reverse();
+             string reversSecond = second.Reverse();           
+             string FullName = reverseFirst + reversSecond;
+             Console.WriteLine(FullName);   
+
+        }
+        else
+        {
+            string reverse = lines.Reverse();
+            Console.WriteLine(reverse+lines);
+        }
+        
+    }   
+}
+
+
